ac3f586ae7746b3b145c5903e86c49add127d39f
ecarbo <enriquecarbo@tutanota.com>
2023-12-29 09:33:56 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,9 @@
+// List of categories for blog posts
+export const CATEGORIES = [
+	'Dermatologia',
+	'Fitomedicina',
+	'General',
+	'Cardiología',
+	'eSalud',
+	'Nutrición'
+] as const
---
+++ src/data/site.config.ts
@@ -0,0 +1,19 @@
+interface SiteConfig {
+	author: string
+	title: string
+	description: string
+	lang: string
+	ogLocale: string
+	shareMessage: string
+	paginationSize: number
+}
+
+export const siteConfig: SiteConfig = {
+	author: 'Enrique Carbó', // Site author
+	title: 'Blog de Salud', // Site title.
+	description: 'Blog dedicado a noticias de salud. No es consejo médico', // Description to display in the meta tags
+	lang: 'es-AR',
+	ogLocale: 'es_AR',
+	shareMessage: 'Coparte este post', // Message to share a post on social media
+	paginationSize: 6 // Number of posts per page
+}
